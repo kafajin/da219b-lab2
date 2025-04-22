4199d7f9eddf0dec5141dfa623e3f09c1c4cc371
Nora <kafaji@gmail.com>
2025-04-22 16:30:13 +0200

    fix: add comments to Project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -2,20 +2,19 @@ const mongoose = require("mongoose");
 
 // Schema for a project in the system
 const projectSchema = new mongoose.Schema({
-  // Unique project identifier (e.g., P001)
   project_code: {
     type: String,
     unique: true,
     required: true,
   },
 
-  // Name of the project (e.g., Website Redesign)
+  // Website Redesign)
   project_name: {
     type: String,
     required: true,
   },
 
-  // Optional description of the project
+  //  description of the project
   project_description: {
     type: String,
   },
